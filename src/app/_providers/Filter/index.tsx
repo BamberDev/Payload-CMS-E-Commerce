5e2733b57b9bd91cbbf8382d37b0c22a936cd4f1
Bamberek <kabaczek99@gmail.com>
2024-07-27 01:30:07 +0200

    created filter context

---
+++ src/app/_providers/Filter/index.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { createContext, useContext, useState } from 'react'
+
+interface FilterContextType {
+  categoryFilters: string[]
+  setCategoryFilters: React.Dispatch<React.SetStateAction<string[]>>
+  sort: string
+  setSort: React.Dispatch<React.SetStateAction<string>>
+}
+
+export const INITIAL_FILTER_DATA = {
+  categoryFilters: [],
+  setCategoryFilters: () => [],
+  sort: '',
+  setSort: () => '',
+}
+
+const FilterContext = createContext<FilterContextType>(INITIAL_FILTER_DATA)
+
+export const FilterProvider = ({ children }: { children: React.ReactNode }) => {
+  const [categoryFilters, setCategoryFilters] = useState([])
+  const [sort, setSort] = useState('-createdAt')
+
+  return (
+    <FilterContext.Provider
+      value={{
+        categoryFilters,
+        setCategoryFilters,
+        sort,
+        setSort,
+      }}
+    >
+      {children}
+    </FilterContext.Provider>
+  )
+}
+
+export const useFilter = () => useContext(FilterContext)
