128dcdcc0f0e7939f693fb736654891077995650
Bamberek <kabaczek99@gmail.com>
2024-07-28 16:19:53 +0200

    updated collection archive

--- src/app/_components/CollectionArchive/index.tsx
+++ src/app/_components/CollectionArchive/index.tsx
@@ -3,7 +3,7 @@
 import React, { Fragment, useCallback, useEffect, useRef, useState } from 'react'
 import qs from 'qs'
 
-import { Category, Product } from '../../../payload/payload-types'
+import { Product } from '../../../payload/payload-types'
 import type { ArchiveBlockProps } from '../../_blocks/ArchiveBlock/types'
 import { useFilter } from '../../_providers/Filter'
 import { Card } from '../Card'
